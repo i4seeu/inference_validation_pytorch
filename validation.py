13f2e0c6510e45a8f95fc4409856bd4154a5fa9a
Noordeen Malango <noordinmalango@gmail.com>
2019-06-04 16:45:40 +0200

    overfitting with dropout

--- validation.py
+++ validation.py
@@ -18,11 +18,80 @@ class Classifier(nn.Module):
         self.fc2 = nn.Linear(256,128)
         self.fc3 = nn.Linear(128,64)
         self.fc4 = nn.Linear(64,10)
+        #Dropout module with 0.2 drop probability
+        self.dropout = nn.Dropout(p=0.2)
     def forward(self, x):
         #make sure input tensor is flattened
         x = x.view(x.shape[0],-1)
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = F.relu(self.fc3(x))
-        x = F.log_softmax(self.fc4(x),dim=1)
+        x = self.dropout(F.relu(self.fc1(x)))
+        x = self.dropout(F.relu(self.fc2(x)))
+        x = self.dropout(F.relu(self.fc3(x)))
+        # output so no dropout here
+        x = F.log_softmax(self.fc4(x), dim=1)
         return x
+model = Classifier()
+images , labels = next(iter(testloader))
+#Get the class probabilities
+ps = torch.exp(model(images))
+
+#make sure the shape is appropriate
+print(ps.shape)
+top_p, top_class = ps.topk(1,dim=1)
+#look at the most likely classes for the first 10 examples
+print(top_class[:10,:])
+
+#compare the outcome and our labels
+equals = top_class == labels.view(*top_class.shape)
+#print(equals)
+#accuracy
+accuracy = torch.mean(equals.type(torch.FloatTensor))
+#print(accuracy.item()*100)
+model = Classifier()
+criterion = nn.NLLLoss()
+optimizer = optim.Adam(model.parameters(), lr=0.003)
+
+epochs = 30
+steps = 0
+
+train_losses, test_losses = [], []
+for e in range(epochs):
+    running_loss = 0
+    for images, labels in trainloader:
+
+        optimizer.zero_grad()
+
+        log_ps = model(images)
+        loss = criterion(log_ps, labels)
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.item()
+
+    else:
+        test_loss = 0
+        accuracy = 0
+
+        # Turn off gradients for validation, saves memory and computations
+        with torch.no_grad():
+            model.eval()
+            for images, labels in testloader:
+                log_ps = model(images)
+                test_loss += criterion(log_ps, labels)
+
+                ps = torch.exp(log_ps)
+                top_p, top_class = ps.topk(1, dim=1)
+                equals = top_class == labels.view(*top_class.shape)
+                accuracy += torch.mean(equals.type(torch.FloatTensor))
+        model.train()
+        train_losses.append(running_loss/len(trainloader))
+        test_losses.append(test_loss/len(testloader))
+
+        print("Epoch: {}/{}.. ".format(e+1, epochs),
+              "Training Loss: {:.3f}.. ".format(running_loss/len(trainloader)),
+              "Test Loss: {:.3f}.. ".format(test_loss/len(testloader)),
+              "Test Accuracy: {:.3f}".format(accuracy/len(testloader)))
+# The graph of trainning loss vs validation loss
+import matplotlib.pyplot as plt
+plt.plot(train_losses, label='Training loss')
+plt.plot(test_losses, label='Validation loss')
+plt.show()
